48e4e56110699241f92537b1244c2d932e30e2a6
Sam Beer <stb41@cam.ac.uk>
2023-02-26 18:26:02 +0000

    next project setup

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <p>
+          This is a paragrapho.
+        </p>
+      </div>
+    </main>
+  )
+}
