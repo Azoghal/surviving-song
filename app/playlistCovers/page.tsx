c4c969a75835e686cb7c92b3b6058e07445e6a02
Sam Beer <stb41@cam.ac.uk>
2023-03-12 00:30:14 +0000

    fixed apos

--- app/playlistCovers/page.tsx
+++ app/playlistCovers/page.tsx
@@ -6,7 +6,7 @@ export default function playlistCovers() {
   return (
     <main>
       <div>
-        <p> Here you'll be able to play with some settings to generate playlist covers from the songs in the playlist using generative AI.</p>
+        <p> Here you&apos;ll be able to play with some settings to generate playlist covers from the songs in the playlist using generative AI.</p>
       </div>
     </main>
   )
