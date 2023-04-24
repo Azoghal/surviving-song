f5e7f407dec41acc5bdf877f22d32f2e4db2f521
Sam Beer <sam.beer208@gmail.com>
2023-04-25 00:11:59 +0100

    finally extracting tokens

--- app/survivingSongs/page.tsx
+++ app/survivingSongs/page.tsx
@@ -4,10 +4,27 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 async function fetchProfile(){
-  const current_url = window.location.href;
-  const url = new URL(current_url)
-  const access_token = url.searchParams.get('access_token');
-  console.log(access_token);
+  const hash = window.location.hash;
+  const hashWithoutHashSymbol = hash.slice(1);
+  // Split the hash into an array of parameter strings
+  const parameterStrings = hashWithoutHashSymbol.split('&');
+  // Create an object to store the parameter values
+  const parameters:any= {};
+
+  // Loop through each parameter string
+  parameterStrings.forEach(paramString => {
+    // Split the parameter string into its key and value parts
+    const parts = paramString.split('=');
+    // Store the key and value in the parameters object
+    parameters[parts[0]] = parts[1];
+  });
+
+  // Access the values of access_token and refresh_token
+  const accessToken = parameters['access_token'];
+  const refreshToken = parameters['refresh_token'];
+  
+  // Use the values in your code
+  console.log(accessToken, refreshToken);
 }
 
 export default function SurvivingSongs() {
