e86b67c8d8b794e93a3a5289f8ad46e68825c232
Sam Beer <sam.beer208@gmail.com>
2023-05-15 19:50:08 +0100

    start

--- app/Login.tsx
+++ app/Login.tsx
@@ -1,10 +1,15 @@
 'use client'
 
+function getAccessToken(){
+    console.log('getAccessToken');
+}
+
+
 export default function Login(){
     // if have spotify cookies then don't show login.
+ 
     return (
-        <a href="https://gcp-test-379914.nw.r.appspot.com/login">
-            Login with Spotify
-        </a>
+        <a href="https://gcp-test-379914.nw.r.appspot.com/login">Login with Spotify</a>
+        //<button onClick={getAccessToken}>GAT</button>
     )
 }
