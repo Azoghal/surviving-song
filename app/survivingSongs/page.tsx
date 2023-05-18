eca979f0c025eaaaebb9636cc8a009ee7a29d06f
Sam Beer <sam.beer208@gmail.com>
2023-05-18 20:19:01 +0100

    log

--- app/survivingSongs/page.tsx
+++ app/survivingSongs/page.tsx
@@ -13,6 +13,7 @@ function getTokens(){
 
 async function fetchPlaylists(){
   const tokens = getTokens();
+  console.log(tokens)
   const result = await fetch("https://api.spotify.com/v1/me/playlists",
   {
     method: "GET", headers: { Authorization: `Bearer ${tokens.accessToken}`}
