c07efe731752c5f9a26f71685591bce44dd3ad0b
Sam Beer <sam.beer208@gmail.com>
2023-05-18 18:19:50 +0100

    survivng songs get from local

--- app/survivingSongs/page.tsx
+++ app/survivingSongs/page.tsx
@@ -6,25 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 let playlists: any;
 
 function getTokens(){
-  const hash = window.location.hash;
-  const hashWithoutHashSymbol = hash.slice(1);
-  // Split the hash into an array of parameter strings
-  const parameterStrings = hashWithoutHashSymbol.split('&');
-  // Create an object to store the parameter values
-  const parameters:any= {};
-
-  // Loop through each parameter string
-  parameterStrings.forEach(paramString => {
-    // Split the parameter string into its key and value parts
-    const parts = paramString.split('=');
-    // Store the key and value in the parameters object
-    parameters[parts[0]] = parts[1];
-  });
-
-  // Access the values of access_token and refresh_token
-  const accessToken = parameters['access_token'];
-  const refreshToken = parameters['refresh_token'];
-  
+  const accessToken = localStorage.getItem('auth-token') || ""
+  const refreshToken = localStorage.getItem('refresh-token') || ""
   return {accessToken:accessToken, refreshToken:refreshToken}
 }
 
